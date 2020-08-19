b799c135f3f2de7ddcbcb8c95c2064b278954df6
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2020-08-19 14:41:35 +0900

    no message

--- js/weather.js
+++ js/weather.js
@@ -6,9 +6,7 @@ function gerWeather(lat, lon) {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
   )
-    .then(function (response) {
-      return response.json();
-    })
+    .then((response) => response.json())
     .then(function (json) {
       console.log(json);
       const temperature = json.main.temp;
